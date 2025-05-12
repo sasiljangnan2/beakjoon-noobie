feb79c7bc5036ea322142bbdf6f909da397a9ac1
sasiljangnan2 <121090375+sasiljangnan2@users.noreply.github.com>
2025-05-13 02:15:49 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.c
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define m 1234567891
 int main(void) 
 {
-	int i, j, L, temp = 0, x;
+	int i, j, L;
+	long long int  x = 1, temp = 0;
 	char str[51];
 	scanf("%d", &L);
 	scanf("%s", &str);
 
 	for (i = 0; i < L; i++) 
 	{
-		x = 1;
-		for (j = 0; j < i; j++)
-		{
-			x *= 31;
-		}
 		temp += x * (str[i] - 'a' + 1);
+		temp %= m;
+		x *= 31;
+		x %= m;;
 	}
-	printf("%d\n", temp % 1234567891);
+	printf("%lld\n", temp);
 	return 0;
 }
 
