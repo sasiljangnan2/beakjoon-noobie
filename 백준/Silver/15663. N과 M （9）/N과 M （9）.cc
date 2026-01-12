c3c617b2dc0b55716765bd202f3da5b0e2fc0366
sasiljangnan2 <121090375+sasiljangnan2@users.noreply.github.com>
2026-01-13 02:33:27 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int arr[9];
+int temp[9];
+int check[9];
+int compare(const void* a, const void* b)
+{
+	return *(int*)a - *(int*)b;
+}
+void dfs(int m, int n, int count)
+{
+	int num = 0;
+	if (count == n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 1; i <= m; i++)
+	{
+		if (check[i] == 0 && num != temp[i-1])
+		{
+			
+			check[i] = 1;
+			num = temp[i - 1];
+			arr[count] = temp[i - 1];
+			dfs(m, n, count + 1);
+			check[i] = 0;
+		}
+	}
+}
+int main()
+{
+	int M, N, i;
+	scanf("%d %d", &M, &N);
+	for (i = 0; i < M; i++) scanf("%d", &temp[i]);
+	qsort(temp, M, sizeof(int), compare);
+	dfs(M, N, 0);
+}
